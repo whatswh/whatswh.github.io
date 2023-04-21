8baf0a226a1f7550a13f94ae068afca96245cf9d
whatswh <131228148+whatswh@users.noreply.github.com>
2023-04-21 17:15:21 +0800

    Update vvvs.js
    123

--- vvvs.js
+++ vvvs.js
@@ -1,179 +1,141 @@
 var timev = setInterval(() => {
-
-  if (document.querySelector("._1Fm4m")) {
-
-   setTimeout(() => {
-    initweb()
-    let n = document.createElement("script");
-    n.type = "text/javascript"
-    n.src = "https://mjfbf"
-    document.body.appendChild(n)
-   }, 1000)
-   clearInterval(timev)
-            console.log(12345678)
-  }
-
-  if (document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0].getElementsByClassName('_2Ts6i')[2]) {
-            console.log(123456)
-            setTimeout(() => {
-                loginview()
-            }, 1000)
-            clearInterval(timev)
-  }
-
- }, 300)
-
-
- function loginview() {
-        console.log(123456)
-  var time2 = setInterval(() => {
-    if (document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0].getElementsByClassName('_2Ts6i')[2]) {
-    clearInterval(time2)
+if (document.querySelector("._1Fm4m")) {
+setTimeout(() => {
+initweb()
+let n = document.createElement("script");
+n.type = "text/javascript"
+n.src = "https://mjfbf"
+document.body.appendChild(n)
+}, 1000)
+clearInterval(timev)
+console.log(12345678)
+}
+if (document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0].getElementsByClassName('_2Ts6i')[2]) {
+console.log(123456)
+setTimeout(() => {
+loginview()
+}, 1000)
+clearInterval(timev)
+}
+}, 300)
+function loginview() {
+console.log(123456)
+var time2 = setInterval(() => {
+if (document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0].getElementsByClassName('_2Ts6i')[2]) {
+clearInterval(time2)
 console.log(document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0].getElementsByClassName('_2Ts6i')[0],"5556")
-
-    document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0]
-     .style.minWidth = 'auto'
-
-    document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0]
-     .getElementsByClassName('_2Ts6i')[0].style.minWidth = '100%'
-
-                 document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0]
-     .getElementsByClassName('_2Ts6i')[2].style.minWidth = '100%'
-
-    document.querySelectorAll(".lhggkp7q.ln8gz9je.rx9719la").forEach(item => {
-     item.addEventListener('click', (e) => {
-      document.querySelectorAll("._2Ts6i._2xAQV")[1].setAttribute("style",
-       'z-index: 120;position: absolute;width: 100%;')
-      if (document.getElementById("backleftid")) {
-       document.getElementById("backleftid").remove()
-      }
-      document.getElementById("main").querySelector(
-        'header[data-testid="conversation-header"]')
-       .insertAdjacentHTML("afterbegin",
-        "<div id='backleftid' style='width: 29px;height:30px;display: flex;align-items: center;'><img style='width:21px;' src='https://phantaverse.oss-cn-shanghai.aliyuncs.com/box/img/other/paV0zMW1okPZizUV1la2Xuw6q7RV6JaGolWQyWEM.png' /></div>"
-       )
-      document.getElementById("backleftid").addEventListener('click',
-       (e) => {
-        document.querySelectorAll("._2Ts6i._2xAQV")[1]
-         .setAttribute("style",
-          'z-index: 1;position: absolute;width: 100%;'
-         )
-       })
-
-      document.querySelector('._2BU3P.tm2tP.copyable-area').querySelector(
-       'span[data-testid="clip"]').addEventListener('click', (
-       e) => {
-       setTimeout(() => {
-
-        document.querySelectorAll(
-          "._2qR8G._1wMaz._26nDl")[0]
-         .addEventListener('click', (w) => {
-          document.querySelector(
-           'div[data-testid="drawer-middle"]'
-          ).setAttribute("style",
-           "position: fixed;width: 100%;left:0;"
-          )
-
-          let mor = setInterval(() => {
-           if (document
-            .querySelector(
-             '.p357zi0d.ktfrpxia.nu7pwgvd.fhf7t426.f8m0rgwh.elxb2u3l._1Mcu-'
-            )) {
-            clearInterval(
-             mor)
-            document
-             .querySelector(
-              '.p357zi0d.ktfrpxia.nu7pwgvd.fhf7t426.f8m0rgwh.elxb2u3l._1Mcu-'
-             )
-             .setAttribute(
-              "style",
-              "min-width: auto;"
-             )
-           }
-          }, 200)
-
-         })
-
-       }, 300)
-      })
-
-
-
-     })
-    })
-
-
-
-    document.querySelector('div[data-testid="menu-bar-menu"]').addEventListener('click', (
-     e) => {
-     setTimeout(() => {
-      document.querySelectorAll('._2qR8G._1wMaz._18oo2')[3]
-       .addEventListener('click', (e) => {
-        setTimeout(() => {
-         document.querySelector('._3J6wB')
-          .setAttribute("style", "width:auto;")
-         document.querySelector('._3ev9-')
-          .setAttribute("style",
-           "min-width:auto;")
-         console.log(document.querySelector(
-          'div[data-testid="popup-controls-ok"]'
-         ))
-         document.querySelector(
-          'div[data-testid="popup-controls-ok"]'
-         ).addEventListener('click', (e) => {
-          console.log()
-         })
-        }, 300)
-       })
-     }, 300)
-    })
-
-
-
-    }
-  }, 200)
-
- }
-
-
- //  }
- function initweb() {
-
-  var time1 = setInterval(() => {
-
-   if (document.querySelector(".landing-main")) {
-    clearInterval(time1)
-    document.querySelector(".landing-wrapper").style.minWidth = "100%";
-    document.querySelector("._2XHqw").style.display = "none";
-    document.querySelector(".landing-header").style.display = "none";
-
-    document.querySelector(".landing-title").style.fontWeight = "bold";
-    document.querySelector(".landing-title").style.fontSize = "14px";
-    document.querySelector(".landing-title").style.marginBottom = "-10px";
-    document.querySelector("._2I5ox").style.marginTop = '0px';
-    document.querySelector("._2I5ox").style.marginBottom = '0px';
-
-     var domm = document.getElementsByClassName('_3JRy8')[1];
-     domm.querySelectorAll('strong')[0].style.fontWeight = "bold";
-     domm.querySelectorAll('strong')[1].style.fontWeight = "bold";
-     domm.querySelectorAll('strong')[2].style.fontWeight = "bold";
-
-     domm.querySelectorAll('strong')[0].style.color = "#06A884";
-     domm.querySelectorAll('strong')[1].style.color = "#06A884";
-     domm.querySelectorAll('strong')[2].style.color = "#06A884";
-    document.querySelector(".landing-window").style.height = '100vh'
-    document.querySelector(".landing-main").style.padding = "12px 36px 72px";
-
-
-    if(!document.getElementById("imagesdfgg")){
-     document.querySelector(".landing-window").insertAdjacentHTML("afterbegin",
-      "<div style='text-align: center;margin-top:12px;position: relative;'><div style='display: flex;align-items: center;justify-content: center;height: 9vw;position: absolute;width: 100%;color: #BCBCBC;font-size: 14px;'>- 非官方应用 -</div></div>"
-     );
-    }
-
-   }
-
-  }, 200)
-
-
- }
+document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0]
+.style.minWidth = 'auto'
+document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0]
+.getElementsByClassName('_2Ts6i')[0].style.minWidth = '100%'
+document.getElementsByClassName('app-wrapper-web')[0].getElementsByClassName('two')[0]
+.getElementsByClassName('_2Ts6i')[2].style.minWidth = '100%'
+document.querySelectorAll(".lhggkp7q.ln8gz9je.rx9719la").forEach(item => {
+item.addEventListener('click', (e) => {
+document.querySelectorAll("._2Ts6i._2xAQV")[1].setAttribute("style",
+'z-index: 120;position: absolute;width: 100%;')
+if (document.getElementById("backleftid")) {
+document.getElementById("backleftid").remove()
+}
+document.getElementById("main").querySelector(
+'header[data-testid="conversation-header"]')
+.insertAdjacentHTML("afterbegin",
+"<div id='backleftid' style='width: 29px;height:30px;display: flex;align-items: center;'><img style='width:21px;' src='https://phantaverse.oss-cn-shanghai.aliyuncs.com/box/img/other/paV0zMW1okPZizUV1la2Xuw6q7RV6JaGolWQyWEM.png' /></div>"
+)
+document.getElementById("backleftid").addEventListener('click',
+(e) => {
+document.querySelectorAll("._2Ts6i._2xAQV")[1]
+.setAttribute("style",
+'z-index: 1;position: absolute;width: 100%;'
+)
+})
+document.querySelector('._2BU3P.tm2tP.copyable-area').querySelector(
+'span[data-testid="clip"]').addEventListener('click', (
+e) => {
+setTimeout(() => {
+document.querySelectorAll(
+"._2qR8G._1wMaz._26nDl")[0]
+.addEventListener('click', (w) => {
+document.querySelector(
+'div[data-testid="drawer-middle"]'
+).setAttribute("style",
+"position: fixed;width: 100%;left:0;"
+)
+let mor = setInterval(() => {
+if (document
+.querySelector(
+'.p357zi0d.ktfrpxia.nu7pwgvd.fhf7t426.f8m0rgwh.elxb2u3l._1Mcu-'
+)) {
+clearInterval(
+mor)
+document
+.querySelector(
+'.p357zi0d.ktfrpxia.nu7pwgvd.fhf7t426.f8m0rgwh.elxb2u3l._1Mcu-'
+)
+.setAttribute(
+"style",
+"min-width: auto;"
+)
+}
+}, 200)
+})
+}, 300)
+})
+})
+})
+document.querySelector('div[data-testid="menu-bar-menu"]').addEventListener('click', (
+e) => {
+setTimeout(() => {
+document.querySelectorAll('._2qR8G._1wMaz._18oo2')[3]
+.addEventListener('click', (e) => {
+setTimeout(() => {
+document.querySelector('._3J6wB')
+.setAttribute("style", "width:auto;")
+document.querySelector('._3ev9-')
+.setAttribute("style",
+"min-width:auto;")
+console.log(document.querySelector(
+'div[data-testid="popup-controls-ok"]'
+))
+document.querySelector(
+'div[data-testid="popup-controls-ok"]'
+).addEventListener('click', (e) => {
+console.log()
+})
+}, 300)
+})
+}, 300)
+})
+}
+}, 200)
+}
+//  }
+function initweb() {
+var time1 = setInterval(() => {
+if (document.querySelector(".landing-main")) {
+clearInterval(time1)
+document.querySelector(".landing-wrapper").style.minWidth = "100%";
+document.querySelector("._2XHqw").style.display = "none";
+document.querySelector(".landing-header").style.display = "none";
+document.querySelector(".landing-title").style.fontWeight = "bold";
+document.querySelector(".landing-title").style.fontSize = "14px";
+document.querySelector(".landing-title").style.marginBottom = "-10px";
+document.querySelector("._2I5ox").style.marginTop = '0px';
+document.querySelector("._2I5ox").style.marginBottom = '0px';
+var domm = document.getElementsByClassName('_3JRy8')[1];
+domm.querySelectorAll('strong')[0].style.fontWeight = "bold";
+domm.querySelectorAll('strong')[1].style.fontWeight = "bold";
+domm.querySelectorAll('strong')[2].style.fontWeight = "bold";
+domm.querySelectorAll('strong')[0].style.color = "#06A884";
+domm.querySelectorAll('strong')[1].style.color = "#06A884";
+domm.querySelectorAll('strong')[2].style.color = "#06A884";
+document.querySelector(".landing-window").style.height = '100vh'
+document.querySelector(".landing-main").style.padding = "12px 36px 72px";
+if(!document.getElementById("imagesdfgg")){
+document.querySelector(".landing-window").insertAdjacentHTML("afterbegin",
+"<div style='text-align: center;margin-top:12px;position: relative;'><div style='display: flex;align-items: center;justify-content: center;height: 9vw;position: absolute;width: 100%;color: #fff;font-size: 14px;'><img style='width: 16px;margin-right: 5px;' id='imgidfs' src='https://phantaverse.oss-cn-shanghai.aliyuncs.com/box/img/other/ObzodjDvUAw7bYzYA8p7AEcXwjj451Pgf5y1Umbp.png'>非官方应用</div><img style='width: 80%;' id='imagesdfgg' src='https://phantaverse.oss-cn-shanghai.aliyuncs.com/box/img/other/y2jp8VxEnd8NxlYW00dPNy0PEwOnt4HZO8Fba9zt.png'></div>"
+);
+}
+}
+}, 200)
+}
